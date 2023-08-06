3e27529ad6e5cc52309068869c41cff19627f311
mohitsingh89 <mohit.singh@akelius.de>
2023-08-06 13:42:21 +0200

    api call

--- myScript.js
+++ myScript.js
@@ -14,4 +14,27 @@ function logSubmit(event) {
    {
        form.addEventListener("submit", logSubmit);
    }
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true)
+
+request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+  
+    if (request.status >= 200 && request.status < 400) {
+      data.forEach((e) => {
+        console.log(e)
+      })
+    } else {
+      console.log('error')
+    }
+  }
+  
+  request.send()
+// Send request
+request.send()
    //console.log(JSON.stringify(document.childNodes,null, 2))
